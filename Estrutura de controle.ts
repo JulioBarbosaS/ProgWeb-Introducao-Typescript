b72e2f9e6e6a626573b1bdee852fedd15132909d
Julio César Barbosa Santos <2023105070017@aluno.ifro.edu.br>
2025-06-04 17:02:53 -0400

    Introdução ao Typescript - parte 2

---
+++ Estrutura de controle.ts
@@ -0,0 +1,12 @@
+// === ESTRUTURAS DE CONTROLE ===   
+
+let idade: number = 16;
+
+if (idade >= 18){
+    //ação
+    console.log('Maior de idade');
+}else if(idade >= 16){
+    console.log('Pode Votar');
+}else{
+    console.log('Menor de idade');
+}
